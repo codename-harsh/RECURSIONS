fdc13b42850e666434380802df6bb152662e6f9b
Harshvardhan Singh <hvsingh46@gmail.com>
2025-11-25 20:33:02 +0530

    Update hanoitower.c

--- hanoitower.c
+++ hanoitower.c
@@ -1,3 +1,5 @@
+//Hanoi tower ka simple logic tha that is jo disks the unhe hame tower 3 yani destination (d) tower pe pahuchana tha with the help of source tower(s) and helping tower(h) .. There were pattern ki agar 3 ka hai to pehle 2 diska helping tower me rkho then i ko destination pe then baki dono ko destination witht he help of source tower . 
+
 #include<stdio.h>
 void tower(int n,char s,char h,char d){
 if(n==0) return;
