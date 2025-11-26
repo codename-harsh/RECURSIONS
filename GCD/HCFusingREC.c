4026e4c33fdd7e29e0a2cf853a098d934eb80eae
Harshvardhan Singh <hvsingh46@gmail.com>
2025-11-26 23:43:03 +0530

    Codes I practiced today  ,i.e. 26/11/2025 .

---
+++ GCD/HCFusingREC.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if(b == 0) 
+        return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    int a = 48, b = 18;
+    printf("GCD(%d, %d) = %d\n", a, b, gcd(a, b));
+    return 0;
+}
